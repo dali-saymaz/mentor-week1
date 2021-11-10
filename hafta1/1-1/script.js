eb262b9af5195515a30a98dd9e1926409e0a3a62
unknown <dalidasy24@gmail.com>
2021-11-10 20:53:02 +0100

    first commit

---
+++ hafta1/1-1/script.js
@@ -0,0 +1,30 @@
+console.log('Merhaba')
+console.log(1234)
+console.log(true)
+console.log([1,2,3])
+// yorum satiri icin ctrl K C yapiniz
+
+//sayisal degeri sona alabiliriz
+// for gibi function gibi kelime olmaz
+//- alt cizgi yada dolarla baslanabilir
+// Buyuk kucuk harf duyarli
+
+// var let const 
+var grup3; 
+var grup3_1;
+var grup3_2;
+
+grup3="biz";
+grup3_1=4;
+grup3_2="kisiyiz";
+
+console.log(grup3);
+console.log(grup3_1);
+console.log(grup3_2);
+
+console.log(grup3+grup3_1+grup3_2);
+
+// aralarda hatalari yakalamak icin
+// console.error('Hata oluştu')
+// console.warn('Bir uyarı oluştu')
+// console.clear()
